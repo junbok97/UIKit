c3acc865fce1fe6d08019ae95337908e212edc5d
junbok97 <junbok97@gmail.com>
2023-05-09 12:58:54 +0900

    ReadMe

---
+++ iOS UIKit/Entitys/Constants/LabelViewController/LabelSettingListCell/LabelLinesCellConstants.swift
@@ -0,0 +1,12 @@
+//
+//  LabelLinesCellConstants.swift
+//  iOS UIKit
+//
+//  Created by 이준복 on 2023/05/08.
+//
+
+import Foundation
+
+struct LabelLinesCellConstants {
+    static let cellId = "LabelLinesCell"
+}
---
+++ iOS UIKit/Presentations/LabelViewcontroller/LabelSettingListCell/LabelLinesCell.swift
@@ -0,0 +1,12 @@
+//
+//  LabelLinesCell.swift
+//  iOS UIKit
+//
+//  Created by 이준복 on 2023/05/08.
+//
+
+import Foundation
+
+final class LabelLinesCell: UITableViewCellReigster {
+    
+}
