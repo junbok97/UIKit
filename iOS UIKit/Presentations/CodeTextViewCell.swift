18ab0d14cea0615f64c6b2dc5dae162347c3c523
junbok97 <junbok97@gmail.com>
2023-05-10 15:15:38 +0900

    CodeCell

---
+++ iOS UIKit/Presentations/CodeTextViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CodeTextViewCell.swift
+//  iOS UIKit
+//
+//  Created by 이준복 on 2023/05/10.
+//
+
+import UIKit
+
+final class CodeTextViewCell: UITableViewCell, UITableViewCellReigster {
+    static var cellId: String = CodeTextViewCellConstants.cellId
+    
+    static var isFromNib: Bool = false
+    
+    
+}
---
+++ iOS UIKit/Presentations/CodeTextViewCellConstants.swift
@@ -0,0 +1,12 @@
+//
+//  CodeTextViewCellConstants.swift
+//  iOS UIKit
+//
+//  Created by 이준복 on 2023/05/10.
+//
+
+import Foundation
+
+struct CodeTextViewCellConstants {
+    static let cellId: String = "CodeTextViewCell"
+}
