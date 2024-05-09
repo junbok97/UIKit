b14cce622d65abb42de05506fd877ce4b6fd8932
junbok97 <junbok97@gmail.com>
2024-05-09 16:05:05 +0900

    feat: ColorTableViewCell.swift Create

---
+++ Projects/DesignSystem/DesignKit/DesignKit/UITableViewCell/ColorTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ColorTableViewCell.swift
+//  DesignKit
+//
+//  Created by 이준복 on 5/9/24.
+//
+
+import UIKit
+
+import PinLayout
+import FlexLayout
+import RxSwift
+import RxCocoa
+
+import Extensions
+
+public final class ColorTableViewCell: BaseTableViewCell {
+    
+    
+}
