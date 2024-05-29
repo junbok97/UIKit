fd550f1d9c0aa23f3de569dc69e2bbf4a1c03ee3
junbok97 <junbok97@gmail.com>
2024-05-29 20:09:38 +0900

    feat: UILabelDemoApp 추가

---
+++ Projects/Presentation/Views/UILabel/UILabelDemoApp/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  UILabelDemoApp
+//
+//  Created by 이준복 on 5/29/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate { }
+
---
+++ Projects/Presentation/Views/UILabel/UILabelDemoApp/SceneDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  SceneDelegate.swift
+//  UILabelDemoApp
+//
+//  Created by 이준복 on 5/29/24.
+//
+
+import UIKit
+import UILabel
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .systemBackground
+        window?.makeKeyAndVisible()
+        
+        
+        window?.rootViewController = UINavigationController(rootViewController: UILabelSettingListViewController())
+    }
+
+    
+}
+
